1af3015165a4c307ed37489e05d38b6d725b6651
Omar Sakr <mero.saker@gmail.com>
2021-06-02 00:12:58 +0200

    Add some styling

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,6 +1,17 @@
+import './loginPage.sass'
+import logo from '../../assets/logo.svg'
+import SelectUser from '../../components/SelectUser'
 
 function LoginPage(){
-    return <h1>LoginScreen</h1>
+    return(
+        <div className='login-Container'>
+            <h1>Would You Rather?</h1>
+            <img src={logo} className='login-logo'/>
+
+            {/* TODO finish the select user component*/}
+            < SelectUser />
+        </div>
+    )
 }
 
 export default LoginPage

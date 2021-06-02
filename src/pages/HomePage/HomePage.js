ec354dd65082a4a278de8083e2eb8eef40cbcbc5
Omar Sakr <mero.saker@gmail.com>
2021-06-03 00:06:08 +0200

    Separate  answered form notAnswered questions

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,14 +7,32 @@ function HomePage(){
     const dispatch = useDispatch()
 
     // Selected questions internal state
-    const [display, setDisplay] = useState('notAnswered') 
+    const [display, setDisplay] = useState('notAnswered')
 
     // Get Questions from API
     useEffect(()=>{
         dispatch(fetchQuestionsAction())
     })
 
+    // Get questions data from redux store
     const questionsData = useSelector(state => state.questions)
+
+    // Get user data from redux store
+    const activeUser = useSelector(state => state.activeUser.activeUserReducer)
+
+    const notAnswered = []
+    const answered= []
+
+    Object.keys(questionsData).map(qId => {
+        if(qId in activeUser.answers){
+            answered.push(questionsData[qId])
+        }else{
+            notAnswered.push(questionsData[qId])
+        }
+    })
+
+    console.log('answered' , answered)
+    console.log('notAnswered', notAnswered)
 
     return(
         <div className='homepage-container'>

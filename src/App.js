81f9bffaf4af850849cd433e33c53849470fd0f2
Omar Sakr <mero.saker@gmail.com>
2021-06-02 20:21:01 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { Provider } from 'react-redux';
 import { store } from './store/store'
 import LoginPage  from './pages/LoginPage'
+import { Route, Router } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <Provider store={store}>
-      <LoginPage />
+      <BrowserRouter>
+        < Route path="/">
+          <LoginPage />
+        </ Route>
+      </BrowserRouter>
+
     </Provider>
 
   );

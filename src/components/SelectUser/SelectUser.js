798b773176a4390b6b654647143c6a0d2c804719
Omar Sakr <mero.saker@gmail.com>
2021-06-02 19:32:49 +0200

    Add user select input for login

--- src/components/SelectUser/SelectUser.js
+++ src/components/SelectUser/SelectUser.js
@@ -1,20 +1,55 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchUsersAction } from "../../store/actionCreator"
+import Select from 'react-select';
+
+
 
 const SelectUser = () => {
     const dispatch = useDispatch()
 
+    // Internal state for active users
+    const [selectedUser, setSelectedUser] = useState()
 
     // Fetch the user data to state
     useEffect(()=>{
-        dispatch(fetchUsersAction())
+        setTimeout(dispatch(fetchUsersAction()), 3000)
     },[])
 
-    // get User data form redux
+    // Get User data form redux
     const usersData = useSelector((state => state.users))
 
-    return <div>Select user</div>
+    // Format the userData to be added in the select
+    const options = Object.keys(usersData).map(user =>({
+                        value:usersData[user].id, label:usersData[user].name
+                    }))
+
+    // Handle selecting user from select
+    const handleSelect = (id) => {
+        setSelectedUser(usersData[id])
+    }
+
+    // Handle button Click
+    const handleLoginButton = () =>{
+        // TODO dispatch
+        console.log(selectedUser)
+    }
+
+    return (
+        <div>
+            <p>Login to continue</p>
+            {/* TODO Add icon beside the name */}
+            <Select
+                options={options}
+                onChange={(evt) => handleSelect(evt.value)}
+                placeholder={"Select User..."}
+            />
+
+            <button onClick={handleLoginButton} >
+                login
+            </button>
+        </div>
+    )
 }
 
 export default SelectUser

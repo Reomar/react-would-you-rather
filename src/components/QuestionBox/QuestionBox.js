ff995fba68324a985b282809710f532c8293495e
Omar Sakr <mero.saker@gmail.com>
2021-06-03 17:45:06 +0200

    Add QuestionBox to view questions

---
+++ src/components/QuestionBox/QuestionBox.js
@@ -0,0 +1,24 @@
+import { useSelector } from 'react-redux'
+import img from '../../assets/avatars/a1.png'
+import './questionBox.sass'
+
+function QuestionBox(props){
+    // Question data
+    const {question} = props
+
+    // Users data from redux
+    const users = useSelector(state => state.users)
+
+    return(
+        <div className='questionBox-container' key={question.id}>
+            <div className='questionBox-header'>
+                <img src={img} width='60px'/>
+                <p>{users[question.author].name} asked, Would You Rather...</p>
+            </div>
+
+            <p>{question.optionOne.text} or {question.optionTwo.text}</p>
+        </div>
+    )
+}
+
+export default QuestionBox

642c46bc5cd8d13b6dd137d36dd323ae8d198499
Omar Sakr <mero.saker@gmail.com>
2021-06-04 14:11:53 +0200

    Change 404 to 401

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -3,7 +3,7 @@ function NotFoundPage(){
     return(
         <div className='would-you-rather not-found'>
             <h1>Would you rather ...</h1>
-            <h1 className='four-o-four'>404</h1>
+            <h1 className='four-o-four'>401</h1>
         </div>
     )
 }

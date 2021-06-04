031e66ac20077cb2f1757c0ea54e1550a5e36cef
Omar Sakr <mero.saker@gmail.com>
2021-06-04 11:53:21 +0200

    Add styles to add question page

--- src/pages/AddQuestionPage/AddQuestionPage.js
+++ src/pages/AddQuestionPage/AddQuestionPage.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useDispatch,useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import { addQuestionAction } from "../../store/actionCreator"
+import './addQuestion.sass'
 
 function AddQuestionPage(){
     const dispatch = useDispatch()
@@ -16,19 +17,23 @@ function AddQuestionPage(){
     }
 
     return(
-        <div>
-            <div className='add-question-Container'>
+        <div className='add-page-container'>
+            <div className='add-question-container'>
                 <div className='add-question-header'>
                     Add a new question
                 </div>
                 <div className='add-question-body'>
                     <p>Would you rather...</p>
                     <input type='text' placeholder='Option one' value={optionOneText} onChange={(e) => setOptionOne(e.target.value)}/>
-                    <p>or</p>
+                    <p className='or'>OR</p>
                     <input type='text' placeholder='Option one' value={optionTwoText} onChange={(e) => setOptionTwo(e.target.value)}/>
                 </div>
 
-                <Link to='/' onClick={handleAddQuestion}>Add Question</Link>
+                <Link to='/' onClick={handleAddQuestion} className='remove-deco'>
+                    <div className='accent-button'>
+                        Add Question
+                    </div>    
+                </Link>
 
             </div>
         </div>

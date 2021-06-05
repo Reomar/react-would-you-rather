27313d1097c442644c1978726d6233964b113187
Omar Sakr <mero.saker@gmail.com>
2021-06-05 15:36:16 +0200

    fetch questions data to redux at the login screen

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,8 +1,20 @@
 import './loginPage.sass'
 import logo from '../../assets/logo.svg'
 import SelectUser from '../../components/SelectUser'
+import { fetchQuestionsAction } from "../../store/actionCreator"
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 
 function LoginPage(){
+        const dispatch = useDispatch()
+        
+        // Get Questions from API
+        useEffect(()=>{
+            dispatch(fetchQuestionsAction())
+        })
+
+
     return(
         <div className='login-Container'>
             <h1 className='would-you-rather'>Would You Rather?</h1>
